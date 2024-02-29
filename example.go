c3b5a4fc6f3f50817d174af51708991aa57fdb1d
Mi <397770083@qq.com>
2024-02-28 20:08:23 -0600

    1

--- example.go
+++ example.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Yeah")
+	var year int
+	var interestRate float64
+	var investmentAmount float64
+	var inflationRate = 6.5
+	fmt.Print("Input your investment amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Input the interest Rate: ")
+	fmt.Scan(&interestRate)
+	fmt.Print("input your investment year: ")
+	fmt.Scan(&year)
+
+	futureValue := investmentAmount * math.Pow(1+interestRate/100,float64(year)) 
+	actualValue := futureValue/math.Pow(1+inflationRate/100,float64(year))
+	println(futureValue)
+	println(actualValue)
 }
